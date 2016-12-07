74a8d455726f869db5bc2f1276cca59f9cf7947d
Takehana Yukinobu <takehana@takehana-macbook-air.local>
2016-12-07 17:13:38 +0900

    Initial Commit

---
+++ gitest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitest
+//
+//  Created by Takehana Yukinobu on 2016/12/07.
+//  Copyright © 2016年 Takehana Yukinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
